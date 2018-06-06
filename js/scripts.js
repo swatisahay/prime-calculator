9f1a60140eaf724bbaf4ee461ddbc89a5aefac75
Swati Sahay <swatiranjan0111@gmail.com>
2018-06-05 19:55:00 -0700

    updated loop and added array for prie calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,23 @@
 $(function() {
 
-$("#submit").click(function(){
-  debugger
-  var index;
-  var prime= $("input#numbers").val();
-  if(prime<2){
-    return false;
-  }
-  for(index = 2 ; index < prime ; index++){
-    if(prime%index === 0){
-      alert("its not a prime number");
+  $("#submit").click(function(){
+    debugger
+    var index;
+    var prime= $("input#numbers").val();
+    var array=[];
+    if(prime<2){
+      return false;
     }
-    
+    for(var i = 3 ;  i < prime ; i ++){
+      for(var j = 2; j < i; j ++ ){
+        if(i%j=== 0){
+          break;
+        }
+      }
+      if(i==j){
+        array.push(i)
+        $("#output").text(array);
+      }
     }
-      alert("it is a prime number");
-
-
-
-
-  // var arrays = palin.split("");
-  // var cloneArray = arrays.slice();
-  // var revArrays= cloneArray.reverse();
-  // if(arrays.toString()===revArrays.toString()){
-  //   $("#output").text(" Yes it is a palindrome")
-  // }
-  // else{
-  //   $("#output").text(" No it is not a palindrome")
-  // }
-
-  // arrays.forEach(function(letter){
-  //   if ((letter==="a")||(letter==="e")||(letter==="i")||(letter==="o")||(letter==="u")){
-  //     output.push("-");
-  //   } else {
-  //     output.push(letter);
-  //   }
-  // });
-  // $("#formOne").hide();
-  //  var total= output.join("");
-  //
 });
 });
